a8cbf956acf186ff9ff3d7f1cd56fd518ffcdc0f
Pablo Alonso Lorente <pablochux@me.com>
2021-09-25 16:56:16 +0200

    First commit, only launched Ironlauncher

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("jobtracker JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/jobtracker";
+
+module.exports = MONGO_URI;
